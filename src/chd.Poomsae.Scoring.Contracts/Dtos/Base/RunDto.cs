f854223e007252d56054b0409e126f755c403110
Christoph Decker <C.Decker@metalldeutsch.com>
2025-04-18 06:28:59 +0200

    Comp 1

--- src/chd.Poomsae.Scoring.Contracts/Dtos/Base/RunDto.cs
+++ src/chd.Poomsae.Scoring.Contracts/Dtos/Base/RunDto.cs
@@ -7,5 +7,7 @@ namespace chd.Poomsae.Scoring.Contracts.Dtos.Base
     public abstract class RunDto
     {
         public JudgeDto Judge { get; set; } = new();
+        public TimeSpan Time { get; set; } = TimeSpan.Zero;
+        public bool Running { get; set; }
     }
 }
---
+++ src/chd.Poomsae.Scoring.UI/Extensions/DecimalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chd.Poomsae.Scoring.UI.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static string ToDisplayString(this decimal value) => value.ToString("n1", CultureInfo.InvariantCulture);
+    }
+}
