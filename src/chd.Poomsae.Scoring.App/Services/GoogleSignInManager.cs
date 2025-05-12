7628fc1dc2e55880802087d2a97e3f4a9bb200d9
Christoph Decker <C.Decker@metalldeutsch.com>
2025-05-12 06:16:01 +0200

    test google auth

--- src/chd.Poomsae.Scoring.App/Services/GoogleSignInManager.cs
+++ src/chd.Poomsae.Scoring.App/Services/GoogleSignInManager.cs
@@ -1,4 +1,9 @@
-﻿using Plugin.Firebase.Auth;
+﻿using chd.Poomsae.Scoring.Contracts.Dtos;
+using chd.UI.Base.Client.Implementations.Authorization;
+using chd.UI.Base.Contracts.Dtos.Authentication;
+using Firebase;
+using Firebase.Auth;
+using Plugin.Firebase.Auth;
 using Plugin.Firebase.Auth.Google;
 using System;
 using System.Collections.Generic;
@@ -8,24 +13,48 @@ using System.Threading.Tasks;
 
 namespace chd.Poomsae.Scoring.App.Services
 {
-    public class GoogleSignInManager
+    public class GoogleSignInManager : ProfileService<Guid, int>
     {
         public GoogleSignInManager()
         {
 
         }
-        public async Task SignIn()
+        private async Task<PSUserDto> SignIn()
         {
-            var user = await CrossFirebaseAuthGoogle.Current.SignInWithGoogleAsync();
-
-            if (user != null)
+            try
             {
-                string name = user.DisplayName;
-                string email = user.Email;
-                string uid = user.Uid;
+                //var user =await CrossFirebaseAuth.Current.SignInWithEmailAndPasswordAsync("christoph.decker@gmx.at","ch3510ri");
+                var user = await CrossFirebaseAuthGoogle.Current.SignInWithGoogleAsync();
 
-                // Hier hast du Zugriff auf den aktuell angemeldeten Google-Nutzer
+                if (user != null)
+                {
+                    string name = user.DisplayName;
+                    string email = user.Email;
+                    string uid = user.Uid;
+                    return new PSUserDto()
+                    {
+                        Username = name,
+                        Email = email,
+                        UID = uid,
+                    };
+                }
             }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        protected override async Task<UserPermissionDto<int>> GetPermissions(UserDto<Guid, int> dto, CancellationToken cancellationToken = default)
+        {
+            return new UserPermissionDto<int>();
+        }
+
+        protected override async Task<UserDto<Guid, int>> GetUser(LoginDto<Guid> dto, CancellationToken cancellationToken = default)
+        {
+            var user = await this.SignIn();
+            return user;
         }
     }
 }
---
+++ src/chd.Poomsae.Scoring.Contracts/Dtos/PSUserDto.cs
@@ -0,0 +1,16 @@
+﻿using chd.UI.Base.Contracts.Dtos.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chd.Poomsae.Scoring.Contracts.Dtos
+{
+    public class PSUserDto : UserDto<Guid, int>
+    {
+        public string UID { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
