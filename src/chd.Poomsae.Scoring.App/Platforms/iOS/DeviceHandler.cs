2d61ff878e632d16783fc42b17f76abc5e7f002d
Christoph Decker <C.Decker@metalldeutsch.com>
2025-06-13 07:33:59 +0200

    Landscape

--- src/chd.Poomsae.Scoring.App/Platforms/iOS/DeviceHandler.cs
+++ src/chd.Poomsae.Scoring.App/Platforms/iOS/DeviceHandler.cs
@@ -24,7 +24,7 @@ namespace chd.Poomsae.Scoring.App.Platforms.iOS
 
         public override async Task RequestLandscape()
         {
-            await this.SetDeviceOrientation(UIInterfaceOrientation.LandscapeLeft);
+            await this.SetDeviceOrientation(UIInterfaceOrientation.LandscapeRight);
             UIViewController.AttemptRotationToDeviceOrientation();
         }
 
