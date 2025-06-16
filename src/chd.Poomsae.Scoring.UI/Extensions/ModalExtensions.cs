e979be3c383cb9b55d92f19026a9bd34696fd471
Christoph Decker <C.Decker@metalldeutsch.com>
2025-06-16 09:49:48 +0200

    ergebnis

--- src/chd.Poomsae.Scoring.UI/Extensions/ModalExtensions.cs
+++ src/chd.Poomsae.Scoring.UI/Extensions/ModalExtensions.cs
@@ -6,6 +6,7 @@ using chd.UI.Base.Components.General.Form;
 using chd.UI.Base.Components.General.Loading;
 using chd.UI.Base.Contracts.Enum;
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -71,9 +72,31 @@ namespace chd.Poomsae.Scoring.UI.Extensions
             return dialogResult;
         }
 
+        public static async Task<string> ShowSmallInputDialog(this IModalHandler modalService, string message, bool isiOS, string placeHolder = null, RenderFragment? childContent = null)
+        {
+            var parameter = new ModalParameters()
+            {
+                {nameof(InputDialog.Placeholder),placeHolder},
+                {nameof(InputDialog.ChildContent),childContent }
+            };
+            var modal = modalService.Show<InputDialog>(message, parameter, new ModalOptions
+            {
+                DisableBackgroundCancel = true,
+                HideCloseButton = true,
+                Class = $"chd-app-modal-small {(isiOS ? "ios" : "")}",
+                PositionCustomClass = "chd-app-modal-position-bottom"
+            });
+            ModalResult res = await modal.Result;
+            if (res.Confirmed && res.Data is string result)
+            {
+                return result;
+            }
+            return string.Empty;
+        }
+
         public static async Task<EDialogResult> ShowYesNoDialog(this IModalHandler modalService, string message, bool isiOS, RenderFragment? childContent = null)
         {
-             var parameter = new ModalParameters()
+            var parameter = new ModalParameters()
             {
                 {nameof(MessageDialog.Buttons),EDialogButtons.YesNo},
                 {nameof(MessageDialog.ChildContent),childContent }
